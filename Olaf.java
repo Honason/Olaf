b810f2f4493aaa7817df56b41c117d699fb262b3
Honason <honason@gmail.com>
2014-09-29 11:09:41 +0200

    First commit with greenfoot

---
+++ Olaf.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Olaf here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Olaf extends Actor
+{
+    /**
+     * Act - do whatever the Olaf wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
