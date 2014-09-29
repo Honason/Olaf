f911d2ec2ef085021720646f8cb6703243647f0e
Honason <honason@gmail.com>
2014-09-29 11:42:18 +0200

    Moving baby

--- Olaf.java
+++ Olaf.java
@@ -8,12 +8,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Olaf extends Actor
 {
+    private int speed = 10;
+    private int weight = 5;
+    
     /**
      * Act - do whatever the Olaf wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        checkKeys();
+        fall();
     }    
+    
+    private void checkKeys() {
+        if (Greenfoot.isKeyDown("left")) {
+            moveLeft();
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            moveRight();
+        }
+    }
+    
+    public void fall(){
+        setLocation (getX(), getY() + weight);
+    }
+    public void moveRight(){
+        setLocation (getX() + speed, getY() );
+    }
+    public void moveLeft(){
+        setLocation (getX() - speed, getY() );
+    }
 }
