abe48e1b946e93fd67bcc0fde4b643cd04d4f66d
Honason <honason@gmail.com>
2014-09-29 16:44:49 +0200

    New Forest class

--- Forest.java
+++ Forest.java
@@ -13,11 +13,21 @@ public class Forest extends World
      * Constructor for objects of class Forest.
      * 
      */
+    private static final String bgImageName = "forest_day.png";  
+    private static final double scrollSpeed = 5;  
+    private static final int picWidth = (new GreenfootImage(bgImageName)).getWidth();
+    private static final Olaf main = new Olaf(); 
+  
+    private GreenfootImage bgImage, bgBase;  
+    private int scrollPosition = 0;  
     public Forest()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-
+        super(900, 600, 1); 
+        setBackground(bgImageName);  
+        bgImage = new GreenfootImage(getBackground());  
+        bgBase = new GreenfootImage(picWidth, getHeight());  
+        bgBase.drawImage(bgImage, 0, 0); 
         prepare();
     }
 
@@ -29,7 +39,24 @@ public class Forest extends World
     {
         Ground ground = new Ground();
         addObject(ground, 308, 300);
-        Olaf olaf = new Olaf();
-        addObject(olaf, 312, 144);
+        addObject(main, 312, 144);
     }
+   public void act() {
+         if(main.getX() == 500) {
+             scrollPosition -= scrollSpeed;
+             if(scrollPosition < -picWidth) scrollPosition = 0;
+             
+            }
+         else if(main.getX() == 200) {
+             scrollPosition += scrollSpeed;
+             if(scrollPosition > 0) scrollPosition = -picWidth;
+            }
+         paint(scrollPosition);  
+   }  
+      
+    private void paint(int position) {  
+        GreenfootImage bg = getBackground();  
+        bg.drawImage(bgBase, position, 0);  
+        bg.drawImage(bgImage, position + picWidth, 0);  
+   }
 }
