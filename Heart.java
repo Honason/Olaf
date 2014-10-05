c866c1a2db5a89df9f3cfb48eb365c90ce28daea
lanatra <anden702@gmail.com>
2014-10-05 15:56:15 +0200

    Fix merge

--- Heart.java
+++ Heart.java
@@ -6,7 +6,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-<<<<<<< HEAD
 public class Heart extends HUD
 {
     /**
